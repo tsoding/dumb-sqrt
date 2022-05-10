55d2f040ce4c061da4921927c5ba7c42dd8c1acc
rexim <reximkut@gmail.com>
2022-05-10 16:47:38 +0700

    [Binary Search] Set X by mouse click

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let EPSILON = 1e-6;
-let MAX_ITERATIONS = 1000;
+let MAX_ITERATIONS = 32;
 let MARKER_COLOR = "#FF4040"
 let AXIS_COLOR = MARKER_COLOR;
 let GRID_COLOR = "#4040FF90"
@@ -12,7 +12,26 @@ let MAX_Y =  10.0;
 let STEP_Y_COUNT = 200
 let STEP_Y = (MAX_Y - MIN_Y)/STEP_Y_COUNT;
 
-function mapToScreen(ctx, x0, y0) {
+// Canvas has two sizes:
+// 1. *Canvas Size*. `[canvas.width, canvas.height]` which is set via the correspoding properties: `<canvas width='800' height='600'>`. This is the logical resolution of the canvas that is used by all of the drawing method of the canvas.
+// 2. *Client Size*. The actual size of the DOM element. You can get this size by doing canvas.getBoundingClientRect().
+// 
+// All of the mouse events are in Client coordinates. This function maps the Client coordinates to Canvas coordinates.
+function mapClientToCanvas(canvas, x0, y0) {
+    const rect = canvas.getBoundingClientRect();
+    const x = (x0 - rect.left) / (rect.right - rect.left) * canvas.width;
+    const y = (y0 - rect.top) / (rect.bottom - rect.top) * canvas.height;
+    return [x, y];
+}
+
+function mapCanvasToWorld(ctx, x0, y0) {
+    // x ∈ [0.0 .. ctx.canvas.width] => x ∈ [0.0 .. 1.0] => x ∈ [MIN_X .. MAX_X] 
+    const x = x0 / ctx.canvas.width * (MAX_X - MIN_X) + MIN_X;
+    const y = (y0 - ctx.canvas.height) * -1.0 / ctx.canvas.height * (MAX_Y - MIN_Y) + MIN_Y;
+    return [x, y];
+}
+
+function mapWorldToCanvas(ctx, x0, y0) {
     // x ∈ [MIN_X .. MAX_X] => x ∈ [0.0 .. 1.0] => x ∈ [0.0 .. ctx.canvas.width]
     const x = (x0 - MIN_X) / (MAX_X - MIN_X) * ctx.canvas.width;
     const y = ctx.canvas.height - (y0 - MIN_Y) / (MAX_Y - MIN_Y) * ctx.canvas.height;
@@ -24,15 +43,15 @@ function renderGrid(ctx) {
 
     for (let x = MIN_X; x <= MAX_X; x += GRID_STEP) {
         ctx.beginPath();
-        ctx.moveTo(...mapToScreen(ctx, x, MIN_Y));
-        ctx.lineTo(...mapToScreen(ctx, x, MAX_Y));
+        ctx.moveTo(...mapWorldToCanvas(ctx, x, MIN_Y));
+        ctx.lineTo(...mapWorldToCanvas(ctx, x, MAX_Y));
         ctx.stroke();
     }
 
     for (let y = MIN_Y; y <= MAX_Y; y += GRID_STEP) {
         ctx.beginPath();
-        ctx.moveTo(...mapToScreen(ctx, MIN_X, y));
-        ctx.lineTo(...mapToScreen(ctx, MAX_X, y));
+        ctx.moveTo(...mapWorldToCanvas(ctx, MIN_X, y));
+        ctx.lineTo(...mapWorldToCanvas(ctx, MAX_X, y));
         ctx.stroke();
     }
 }
@@ -41,13 +60,13 @@ function renderAxis(ctx) {
     ctx.strokeStyle = AXIS_COLOR;
 
     ctx.beginPath();
-    ctx.moveTo(...mapToScreen(ctx, MIN_X, 0.0));
-    ctx.lineTo(...mapToScreen(ctx, MAX_X, 0.0));
+    ctx.moveTo(...mapWorldToCanvas(ctx, MIN_X, 0.0));
+    ctx.lineTo(...mapWorldToCanvas(ctx, MAX_X, 0.0));
     ctx.stroke();
 
     ctx.beginPath();
-    ctx.moveTo(...mapToScreen(ctx, 0.0, MIN_Y));
-    ctx.lineTo(...mapToScreen(ctx, 0.0, MAX_Y));
+    ctx.moveTo(...mapWorldToCanvas(ctx, 0.0, MIN_Y));
+    ctx.lineTo(...mapWorldToCanvas(ctx, 0.0, MAX_Y));
     ctx.stroke();
 }
 
@@ -58,19 +77,27 @@ function renderPlot(ctx) {
     }
 }
 
+function strokeLine(ctx, p1, p2)
+{
+    ctx.beginPath();
+    ctx.moveTo(...p1);
+    ctx.lineTo(...p2);
+    ctx.stroke();
+}
+
 function renderDiagonal(ctx) {
     ctx.strokeStyle = MARKER_COLOR;
     ctx.beginPath();
-    ctx.moveTo(...mapToScreen(ctx, 0, 0));
+    ctx.moveTo(...mapWorldToCanvas(ctx, 0, 0));
     {
         const a = Math.min(MAX_X, MAX_Y);
-        ctx.lineTo(...mapToScreen(ctx, a, a));
+        ctx.lineTo(...mapWorldToCanvas(ctx, a, a));
     }
     ctx.stroke();
 }
 
 function renderMarker(ctx, x0, y0) {
-    const [x, y] = mapToScreen(ctx, x0, y0);
+    const [x, y] = mapWorldToCanvas(ctx, x0, y0);
     ctx.fillStyle = MARKER_COLOR;
     ctx.fillRect(x - MARKER_SIZE, y - MARKER_SIZE, 2*MARKER_SIZE, 2*MARKER_SIZE);
 }
@@ -83,16 +110,18 @@ function renderBinarySearch(ctx, state) {
     renderPlot(ctx);
     renderDiagonal(ctx);
 
-    ctx.fillStyle = "#50FF5030";
-    let [y0, y1] = state.trace[state.traceIndex];
-    {
-        let [rx0, ry0] = mapToScreen(ctx, 0, y1);
-        let [rx1, ry1] = mapToScreen(ctx, MAX_X, y0);
+    for (let i = 0; i < state.trace.length; ++i) {
+        const alpha = Math.floor(1/state.trace.length*(i + 1)*255);
+        ctx.fillStyle = "#50FF50"+alpha.toString(16).padStart(2, 0);
+        const [y0, y1] = state.trace[i];
+        const [rx0, ry0] = mapWorldToCanvas(ctx, 0, y1);
+        const [rx1, ry1] = mapWorldToCanvas(ctx, MAX_X, y0);
         ctx.fillRect(rx0, ry0, rx1 - rx0, ry1 - ry0);
     }
-    renderMarker(ctx, 0, y0);
-    renderMarker(ctx, 0, y1);
+
     renderMarker(ctx, state.xArg, 0);
+    ctx.strokeStyle = MARKER_COLOR;
+    strokeLine(ctx, mapWorldToCanvas(ctx, state.xArg, MIN_Y), mapWorldToCanvas(ctx, state.xArg, MAX_Y));
 }
 
 function renderNewtonMethod(ctx, state) {
@@ -106,8 +135,8 @@ function renderNewtonMethod(ctx, state) {
     let y = state.trace[state.traceIndex];
     ctx.strokeStyle = MARKER_COLOR;
     ctx.beginPath();
-    ctx.moveTo(...mapToScreen(ctx, MIN_X, y));
-    ctx.lineTo(...mapToScreen(ctx, MAX_X, y));
+    ctx.moveTo(...mapWorldToCanvas(ctx, MIN_X, y));
+    ctx.lineTo(...mapWorldToCanvas(ctx, MAX_X, y));
     ctx.stroke();
 }
 
@@ -141,7 +170,8 @@ function binarySearchSqrt(x, trace)
     return y0;
 }
 
-let binaryCtx = document.getElementById("app-binary-search").getContext("2d");
+let binaryElem = document.getElementById("app-binary-search");
+let binaryCtx = binaryElem.getContext("2d");
 let binaryState = {
     trace: [],
     traceIndex: 0,
@@ -149,8 +179,23 @@ let binaryState = {
 };
 binarySearchSqrt(binaryState.xArg, (s) => binaryState.trace.push(s));
 renderBinarySearch(binaryCtx, binaryState);
+binaryElem.addEventListener("click", (e) => {
+    const state = binaryState;
+    const sqrt = binarySearchSqrt;
+    const render = renderBinarySearch;
+    const ctx = binaryCtx;
+    const p = mapCanvasToWorld(binaryCtx, 
+        ...mapClientToCanvas(binaryElem, e.clientX, e.clientY)
+    );
+    state.xArg = p[0];
+    state.trace.length = 0;
+    sqrt(state.xArg, (s) => state.trace.push(s));
+    state.traceIndex = 0;
+    render(ctx, state);
+});
 
-let newtonCtx = document.getElementById("app-newton-method").getContext("2d");
+let newtonElem = document.getElementById("app-newton-method");
+let newtonCtx = newtonElem.getContext("2d");
 let newtonState = {
     trace: [],
     traceIndex: 0,
@@ -158,52 +203,3 @@ let newtonState = {
 };
 newtonMethodSqrt(newtonState.xArg, (s) => newtonState.trace.push(s))
 renderNewtonMethod(newtonCtx, newtonState);
-
-console.log(newtonMethodSqrt(newtonState.xArg));
-console.log(newtonState.trace);
-console.log(binaryState.trace);
-
-document.addEventListener("keydown", (e) => {
-    const ctx = binaryCtx;
-    const render = renderBinarySearch;
-    const state = binaryState;
-    const sqrt = binarySearchSqrt;
-
-    // const ctx = newtonCtx;
-    // const render = renderNewtonMethod;
-    // const state = newtonState;
-    // const sqrt = newtonMethodSqrt;
-
-    switch (e.key) {
-    case "ArrowUp": {
-        if (state.traceIndex > 0) {
-            state.traceIndex -= 1;
-            render(ctx, state);
-        }
-    } break;
-    case "ArrowDown": {
-        if (state.traceIndex < state.trace.length - 1) {
-            state.traceIndex += 1;
-            render(ctx, state);
-        }
-    } break;
-    case "ArrowRight": {
-        if (state.xArg < MAX_Y * MAX_Y) {
-            state.xArg += 1;
-            state.trace.length = 0;
-            sqrt(state.xArg, (s) => state.trace.push(s));
-            state.traceIndex = 0;
-            render(ctx, state);
-        }
-    } break;
-    case "ArrowLeft": {
-        if (state.xArg > 0) {
-            state.xArg -= 1;
-            state.trace.length = 0;
-            sqrt(state.xArg, (s) => state.trace.push(s));
-            state.traceIndex = 0;
-            render(ctx, state);
-        }
-    } break;
-    }
-})
